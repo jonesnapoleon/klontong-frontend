c77a5c8756b28e78ffa216bf7c9bbb7751840f94
Jones Napoleon Autumn <jones.autumn@seamoney.com>
2023-01-20 00:11:50 +0700

    feat: add Footer component

---
+++ src/components/skeleton/Footer.tsx
@@ -0,0 +1,11 @@
+import { Layout } from 'antd'
+
+const Footer = () => {
+  return (
+    <Layout.Footer style={{ textAlign: 'center' }}>
+      Brik ©2023 Created by Jones Napoleon
+    </Layout.Footer>
+  )
+}
+
+export default Footer
