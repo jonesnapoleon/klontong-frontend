68908682a0158c9534976b8ae5c2ac635608563b
Jones Napoleon Autumn <jones.autumn@seamoney.com>
2023-01-20 00:12:21 +0700

    feat: modify API and type

--- src/services/product.ts
+++ src/services/product.ts
@@ -1,17 +1,43 @@
 import { CONSTANT_RESOURCE_MAPPING } from '@/utils/constants'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import abstractFetcher, { IError } from './api/fetcher'
-import { IProductDataResponse } from './type'
+import { IProductDataResponse, IProductDatumResponse } from './type'
 
 export const useProductQuery = () => {
   return useQuery<IProductDataResponse, IError>(
     [CONSTANT_RESOURCE_MAPPING],
     async () => {
       const data = await abstractFetcher<IProductDataResponse>({
-        url: CONSTANT_RESOURCE_MAPPING,
+        url: `${CONSTANT_RESOURCE_MAPPING}?rq_uid=NX5Qp7z4eBhUaZWofEAxFSkZ7YM2`,
         method: 'GET',
       })
       return data
+    }
+  )
+}
+
+export const useProductMutation = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation<IProductDatumResponse, IError, IProductDatumResponse>(
+    async (data: IProductDatumResponse) => {
+      const result = await abstractFetcher<IProductDatumResponse>({
+        url: CONSTANT_RESOURCE_MAPPING,
+        method: 'POST',
+        data,
+      })
+      return result
+    },
+    {
+      onSuccess: (newProduct: IProductDatumResponse) => {
+        queryClient.setQueryData<IProductDatumResponse>(
+          [CONSTANT_RESOURCE_MAPPING],
+          (currentProducts: IProductDataResponse) => [
+            ...(currentProducts ?? []),
+            newProduct,
+          ]
+        )
+      },
     }
   )
 }
--- src/services/type.ts
+++ src/services/type.ts
@@ -1,3 +1,7 @@
 import { Product } from '@/providers/Product/type'
 
-export type IProductDataResponse = Product[]
+export type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> &
+  U[keyof U]
+
+export type IProductDatumResponse = AtLeastOne<Product>
+export type IProductDataResponse = IProductDatumResponse[]
