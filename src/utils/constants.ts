039937ede393a815f69a82ecc69c86860d274bc4
Jones Napoleon Autumn <jones.autumn@seamoney.com>
2023-01-20 01:23:29 +0700

    chore: add FRONTEND_URL constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,3 +3,7 @@ export const CONSTANT_COMPANY_LOGO = 'https://brik.id/logos/brikLogo1.svg'
 export const CONSTANT_BASEPATH = process.env.NEXT_PUBLIC_API_BASEPATH as string
 export const CONSTANT_RESOURCE_MAPPING = process.env
   .NEXT_PUBLIC_RESOURCE_MAPPING as string
+
+export const CONSTANT_FRONTEND_URL = {
+  BASE_PATH: '/',
+}
