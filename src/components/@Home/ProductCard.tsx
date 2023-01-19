48f597eed3cbb54db6c4012ed4ceb0a20ad04622
Jones Napoleon Autumn <jones.autumn@seamoney.com>
2023-01-20 01:23:50 +0700

    feat: add Home page and Product Card

---
+++ src/components/@Home/ProductCard.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable @next/next/no-img-element */
+import { Product } from '@/providers/Product/type'
+import { AtLeastOne } from '@/services/type'
+import { formatIDR } from '@/utils/transformers'
+import { Card, Col } from 'antd'
+import { useRouter } from 'next/router'
+
+type IProductCardProps = {
+  product: AtLeastOne<Product>
+}
+
+const ProductCard = ({ product }: IProductCardProps) => {
+  const { push } = useRouter()
+
+  return (
+    <Col xs={24} md={12} lg={6} xl={4}>
+      <Card
+        onClick={() => push(`${product._id}`)}
+        hoverable
+        style={{ width: '100%', height: '100%', position: 'relative' }}
+        cover={
+          <img
+            alt={product.description ?? product.name ?? ''}
+            src={product.image!}
+          />
+        }
+      >
+        <Card.Meta
+          title={product.name}
+          description={formatIDR(product.price!)}
+        />
+      </Card>
+    </Col>
+  )
+}
+
+export default ProductCard
--- src/components/@Home/index.tsx
+++ src/components/@Home/index.tsx
@@ -1,9 +1,17 @@
 import { useProduct } from '@/providers/Product/ProductProvider'
+import { Row } from 'antd'
+import ProductCard from './ProductCard'
 
 const AllProducts = () => {
   const { products } = useProduct()
-  console.log(products)
-  return <div>Home</div>
+
+  return (
+    <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 24]}>
+      {products.map((product) => (
+        <ProductCard key={product._id} product={product} />
+      ))}
+    </Row>
+  )
 }
 
 export default AllProducts
