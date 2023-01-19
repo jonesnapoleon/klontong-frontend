be43c59c060397608b07ed2fe5eb76ff25ef3333
Jones Napoleon Autumn <jones.autumn@seamoney.com>
2023-01-19 11:17:28 +0700

    chore: add constant file reference from env

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const CONSTANT_COMPANY_LOGO = 'https://brik.id/logos/brikLogo1.svg'
+
+export const CONSTANT_BASEPATH = process.env.NEXT_PUBLIC_API_BASEPATH as string
+export const CONSTANT_RESOURCE_MAPPING = process.env
+  .NEXT_PUBLIC_RESOURCE_MAPPING as string
