df625fc678d7fc2f0eafcf39609df374c0e791a5
Jones Napoleon Autumn <jones.autumn@seamoney.com>
2023-01-19 11:17:07 +0700

    chore: delete unused file, add app structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
+import ProductProvider from '@/providers/Product/ProductProvider'
+import { queryClient } from '@/services/queryclient'
+import { QueryClientProvider } from '@tanstack/react-query'
 import 'antd/dist/reset.css'
 
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ProductProvider>
+        <Component {...pageProps} />
+      </ProductProvider>
+    </QueryClientProvider>
+  )
 }
