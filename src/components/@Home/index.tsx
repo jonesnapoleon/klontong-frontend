fa111883c985afde001db659091a99bf38ac19d2
Jones Napoleon Autumn <jones.autumn@seamoney.com>
2023-01-20 17:09:57 +0700

    style: UX - when no product is currently available

--- src/components/@Home/index.tsx
+++ src/components/@Home/index.tsx
@@ -1,16 +1,24 @@
 import { useProduct } from '@/providers/Product/ProductProvider'
-import { Row } from 'antd'
+import { Empty, Row } from 'antd'
+import AddProductButton from '../@AddProduct/AddProductButton'
 import ProductCard from './ProductCard'
 
 const AllProducts = () => {
   const { displayedProducts } = useProduct()
 
-  return (
+  return displayedProducts.length === 0 ? (
     <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 24]}>
       {displayedProducts.map((product) => (
         <ProductCard key={product._id} product={product} />
       ))}
     </Row>
+  ) : (
+    <Empty
+      image={Empty.PRESENTED_IMAGE_SIMPLE}
+      description={<span>Currently no product available</span>}
+    >
+      <AddProductButton />
+    </Empty>
   )
 }
 
